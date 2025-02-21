003c6a0b92e164fee274778109bf73b80939aeff
Diego Giraldo <dgiraldogaleano@gmail.com>
2025-02-21 15:01:01 -0500

    Actualizacion de los componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+
+const defaultTodos = [
+    { text: 'Cortar cebolla', completed: true },
+    { text: 'Tomar el Curso de Intro a React.js', completed: false },
+    { text: 'Llorar con la Llorona', completed: false },
+    { text: 'LALALALALA', completed: false },
+    { text: 'Usar estados derivados', completed: true },
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = React.useState(defaultTodos);
+    const [searchValue, setSearchValue] = React.useState('');
+
+    const completedTodos = todos.filter(
+        todo => !!todo.completed
+    ).length;
+    const totalTodos = todos.length;
+
+    console.log( searchValue);
+
+    return (
+        <>
+            <TodoCounter
+                completed={completedTodos}
+                total={totalTodos}
+            />
+            <TodoSearch
+                searchValue={searchValue}
+                setSearchValue={setSearchValue}
+            />
+
+            <TodoList>
+                {defaultTodos.map(todo => (
+                    <TodoItem
+                        key={todo.text}
+                        text={todo.text}
+                        completed={todo.completed}
+                    />
+                ))}
+            </TodoList>
+
+            <CreateTodoButton />
+        </>
+    );
 }
 
 export default App;
---
+++ src/TodoItem.js
@@ -0,0 +1,21 @@
+import './TodoItem.css'
+
+function TodoItem(props) {
+    return (
+        <>
+            <li>
+                <span>
+                    v
+                </span>
+                <p>
+                    {props.text}
+                </p>
+                <span>
+                    x
+                </span>
+            </li>
+        </>
+    )
+}
+
+export {TodoItem};
